84c83db8028abf17a8f3a8334ee1c23d4ef58d96
govtgeek <26489473+GovtGeek@users.noreply.github.com>
2026-05-18 16:34:07 -0700

    Added TBC check for swift flight form enablement

--- OutfitterScripting.lua
+++ OutfitterScripting.lua
@@ -142,8 +142,8 @@ Outfitter.ScriptModules.DruidShapeshift.Events =
 	Tree = "TREE_FORM",
 	Flight = "SWIFT_FLIGHT_FORM" --DAC
 }
--- Add Swift Flight Form if we're in Classic Wrath
-if Outfitter:IsClassicWrath() then
+-- Add Swift Flight Form if we're in Classic TBC or Wrath
+if Outfitter.IsClassicTBC or Outfitter.IsClassicWrath then
 	Outfitter.ScriptModules.DruidShapeshift.Events.Flight = "SWIFT_FLIGHT_FORM"
 end
 
@@ -1771,8 +1771,8 @@ end
 	}
 }
 
--- Add Classic Wrath
-if Outfitter:IsClassicWrath() then
+-- Add Classic TBC and Wrath
+if Outfitter.IsClassicTBC or Outfitter.IsClassicWrath then
 	table.insert(Outfitter.PresetScripts, {
 		Name = Outfitter.cDruidSwiftFlightForm, --DAC
 		ID = "Flight",
